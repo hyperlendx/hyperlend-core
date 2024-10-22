7203d4fe15aefb0fc925b1da5722ef73335761f5
fbslo <admin@fbslo.net>
2024-10-22 15:14:59 +0200

    match chainlink interface formatting

--- contracts/dependencies/chainlink/AggregatorInterface.sol
+++ contracts/dependencies/chainlink/AggregatorInterface.sol
@@ -4,11 +4,16 @@ pragma solidity ^0.8.0;
 
 interface AggregatorInterface {
     function latestAnswer() external view returns (int256);
+
     function latestTimestamp() external view returns (uint256);
+
     function latestRound() external view returns (uint256);
+
     function getAnswer(uint256 roundId) external view returns (int256);
+
     function getTimestamp(uint256 roundId) external view returns (uint256);
-    function description() external view returns (string memory);
+
     event AnswerUpdated(int256 indexed current, uint256 indexed roundId, uint256 updatedAt);
+
     event NewRound(uint256 indexed roundId, address indexed startedBy, uint256 startedAt);
 }
